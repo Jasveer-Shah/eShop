a966e951e89bdcc44a29c896e078cb41ebf26860
Marek Carter <cartermarek@gmail.com>
2021-08-04 20:01:36 -0400

    ADD: footer elements

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,22 @@ import React from 'react'
 
 function Footer() {
     return (
-        <div>
-           <h1>Footer</h1> 
+        <div className="footer__container">
+           <h1>Footer</h1>
+            <div>
+                <div>
+                    <a href="">Link 1</a>
+                    <a href="">Line 2</a>
+                    <a href="">Link 3</a>
+                    </div>
+                <div>
+                    <img src="" alt="fb" />
+                    <img src="" alt="twitter" />
+                    <img src="" alt="instagram" />
+                    <img src="" alt="pinterest" />
+                </div>
+                <div>COPYRIGHT 2021</div>
+            </div>
         </div>
     )
 }
