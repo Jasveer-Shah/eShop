65791ddb09ca7fd284ac36be634d8b404866ce4c
leshleman999 <eshleman60@gmail.com>
2021-08-19 16:45:44 -0700

    testing git

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './App.css';
 import Header from './components/Header.jsx'
 import Footer from './components/Footer.jsx'
+//note to test github
 
 function App() {
   return (
