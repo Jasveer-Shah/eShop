c45162ba08758e773317782c68a804641db4960d
jasveer shah <talkjasveer@gmail.com>
2021-08-19 20:36:01 -0400

    ADD: WelcomeImage

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from 'react'
 import './App.css';
 import Header from './components/Header.jsx'
 import Footer from './components/Footer.jsx'
+import WelcomeImage from './components/WelcomeImage';
 //note to test github
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <WelcomeImage />
       <Footer />
+
     </div>
   );
 }
---
+++ src/components/WelcomeImage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function WelcomeImage() {
+    return (
+        <div>
+            <h1>Welcome everyone</h1>
+        </div>
+    )
+}
+
+export default WelcomeImage
